733a32dc1fdd31181e9b87580f0868e37d23a495
kane.chong <kane.chong@lytx.com>
2017-02-09 22:47:31 -0800

    Added some protractor configs. Need to fix issue where jasmine definitions are not being picked up by ts-node.

---
+++ config/protractor.conf.js
@@ -0,0 +1,38 @@
+require('ts-node/register');
+
+exports.config = {
+   baseUrl: 'http://localhost:3000/',
+
+   specs: [
+      'e2e/**/*.spec.ts'
+   ],
+
+   exclude: [],
+
+   framework: 'jasmine2',
+
+   allScriptsTimeout: 110000,
+
+   jasmineNodeOpts: {
+      showTiming: true,
+      showColors: true,
+      isVerbose: false,
+      includeStackTrace: false,
+      defaultTimeoutInterval: 400000
+   },
+
+   directConnect: true,
+
+   capabilities: {
+      'browserName': 'chrome',
+      'chromeOptions': {
+         'args': ['show-fps-counter=true']
+      }
+   },
+
+   onPrepare: function() {
+      browser.ignoreSynchronization = true;
+   },
+
+   useAllAngular2AppRoots: false
+};
